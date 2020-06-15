d8e534b8ddccb65556084b0581e32b96e84041df
gafetinov <artem.gafetinov@gmail.com>
2020-06-15 18:55:05 +0500

    refactor

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import commander from 'commander';
 
 const { Command } = commander;
 
-export default () => {
-  const program = new Command();
+const initProgram = (program) => {
   program.version('0.1.0', '-v, --version');
   program.description('Compares two configuration files and shows a difference.');
   program.arguments('<filepath1> <filepath2>');
   program.option('-f, --format [type]', 'output format');
+};
+
+export default () => {
+  const program = new Command();
+  initProgram(program);
   program.parse(process.argv);
 };
