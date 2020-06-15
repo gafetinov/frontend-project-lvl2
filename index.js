f42ea52fa157d5f810c5c8d1552ae62d7b2b32d7
gafetinov <artem.gafetinov@gmail.com>
2020-06-15 18:43:52 +0500

    Change version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Command } = commander;
 
 export default () => {
   const program = new Command();
-  program.version('0.0.1', '-v, --version');
+  program.version('0.1.0', '-v, --version');
   program.description('Compares two configuration files and shows a difference.');
   program.arguments('<filepath1> <filepath2>');
   program.option('-f, --format [type]', 'output format');
