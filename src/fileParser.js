9a93f2551d820f99074837c3981a1c8dc20ce912
gafetinov <artem.gafetinov@gmail.com>
2020-06-20 22:25:47 +0500

    Support .yml files

--- src/fileParser.js
+++ src/fileParser.js
@@ -5,6 +5,7 @@ export default (fileContent, format) => {
     case '.json':
       return JSON.parse(fileContent);
     case '.yaml':
+    case '.yml':
       return yaml.safeLoad(fileContent);
     default:
       throw new Error(`Format "${format}" not supported`);
