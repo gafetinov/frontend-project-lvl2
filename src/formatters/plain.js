0c38fdf6a47b7a5b2132519bd51cebb4fbb2e2a5
gafetinov <artem.gafetinov@gmail.com>
2020-07-16 23:24:02 +0500

    Write plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,53 @@
-const plain = (compare) => {};
+import { fieldStatuses } from '../shared.js';
+
+const getValueString = (value) => {
+  switch (typeof value) {
+    case 'object':
+      return '[complex value]';
+    case 'string':
+      return `'${value}'`;
+    default:
+      return String(value);
+  }
+};
+
+const getRemovedStr = (path) => `Property '${path}' was removed`;
+const getAddedStr = (path, value) => `Property '${path}' was added with value: ${getValueString(value)}`;
+const getModifiedStr = (path, value, prev) => `Property '${path}' was updated. From ${getValueString(prev)} to ${getValueString(value)}`;
+
+const getCompareString = (path, compare) => {
+  const { status, value, prev } = compare;
+  switch (status) {
+    case fieldStatuses.deleted:
+      return getRemovedStr(path);
+    case fieldStatuses.added:
+      return getAddedStr(path, value);
+    case fieldStatuses.modified:
+      return getModifiedStr(path, value, prev);
+    case fieldStatuses.unmodified:
+      return undefined;
+    default:
+      throw Error(`Not allowed gen compare string with status ${status}`);
+  }
+};
+
+const plain = (compare, path = '') => {
+  const { value, status } = compare;
+  if (status === fieldStatuses.iterable) {
+    return value
+      .map((element, i) => plain(element, `${path}[${i}]`))
+      .filter((x) => Boolean(x))
+      .join('\n');
+  }
+  if (status !== fieldStatuses.deep) {
+    return getCompareString(path, compare);
+  }
+  const lines = [];
+  const keys = Object.keys(value);
+  keys.forEach((key) => {
+    lines.push(plain(value[key], `${path}${path ? '.' : ''}${key}`));
+  });
+  return lines.filter((x) => Boolean(x)).join('\n');
+};
 
 export default plain;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import { compareFiles } from './comparator.js';
 import cliProgram from './cli-program.js';
 import stylish from './formatters/stylish.js';
+import { outputFormats } from './shared.js';
+import plain from './formatters/plain.js';
 
 
-const genDiff = (a, b) => stylish(compareFiles(a, b));
+const genDiff = (a, b, format) => {
+  const diff = compareFiles(a, b);
+  if (format === outputFormats.plain) {
+    return plain(diff);
+  }
+  return stylish(diff);
+};
 
 export const cli = () => {
   const program = cliProgram();
   const files = program.parse(process.argv).args;
-  console.log(genDiff(...files));
+  console.log(genDiff(...files, program.format));
 };
 
 export default genDiff;
