2a3e51ca5f6f3d7436e33655e9bf32a0fb031d53
gafetinov <artem.gafetinov@gmail.com>
2020-07-20 20:19:45 +0500

    Handle case with unknown output format

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { outputFormats } from './shared.js';
 import plain from './formatters/plain.js';
 
 
-const genDiff = (a, b, format) => {
+const genDiff = (a, b, format = 'json') => {
   const diff = compareFiles(a, b);
   if (format === outputFormats.plain) {
     return plain(diff);
@@ -13,7 +13,10 @@ const genDiff = (a, b, format) => {
   if (format === outputFormats.json) {
     return JSON.stringify(diff, null, '  ');
   }
-  return stylish(diff);
+  if (format === outputFormats.stylish) {
+    return stylish(diff);
+  }
+  throw new Error(`Unknown format ${format}`);
 };
 
 export const cli = () => {
