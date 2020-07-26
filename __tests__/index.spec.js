2c9a015e91a67b6fed4b7455d07200d086e2c5c7
gafetinov <artem.gafetinov@gmail.com>
2020-07-26 17:02:37 +0500

    Update tests in index.spec.js

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,182 +1,53 @@
+import fs from 'fs';
 import genDiff from '../src';
 import { outputFormats } from '../src/shared.js';
 
 describe('compareFiles', () => {
-  const rightCompare = [
-    '{',
-    '    host: hexlet.io',
-    '  - timeout: 50',
-    '  + timeout: 20',
-    '  - proxy: 123.234.53.22',
-    '  - follow: false',
-    '  + verbose: true',
-    '}',
-  ].join('\n');
-
-  const genCompare = (fileNameEnd, outputFormat = outputFormats.stylish) => genDiff(
+  const genCompare = (fileNameEnd, outputFormat) => genDiff(
     `${__dirname}/../__fixtures__/before${fileNameEnd}`,
     `${__dirname}/../__fixtures__/after${fileNameEnd}`,
     outputFormat,
   );
 
-  test('should compare JSON files', () => {
-    expect(genCompare('.json')).toBe(rightCompare);
+  describe('should compare files with different formats', () => {
+    const expectedJsonString = fs.readFileSync(
+      `${__dirname}/../__fixtures__/json.txt`,
+      'utf-8',
+    );
+
+    test('should compare JSON files', () => {
+      expect(genCompare('.json')).toBe(expectedJsonString);
+    });
+
+    test('should compare YAML files', () => {
+      expect(genCompare('.yaml')).toBe(expectedJsonString);
+      expect(genCompare('.yml')).toBe(expectedJsonString);
+    });
+
+    test('should compare .ini files', () => {
+      expect(genCompare('.ini')).toBe(expectedJsonString);
+    });
   });
-  test('should compare YAML files', () => {
-    expect(genCompare('.yaml')).toBe(rightCompare);
-    expect(genCompare('.yml')).toBe(rightCompare);
-  });
-  test('should compare .ini files', () => {
-    expect(genCompare('.ini')).toBe(rightCompare);
-  });
-  test('should generate volume compare', () => {
-    const rightDeepCompare = [
-      '{',
-      '    common: {',
-      '        setting1: Value 1',
-      '      - setting2: 200',
-      '      - setting3: true',
-      '      + setting3: {',
-      '            key: value',
-      '        }',
-      '        setting6: {',
-      '            key: value',
-      '          + ops: vops',
-      '        }',
-      '      + follow: false',
-      '      + setting4: blah blah',
-      '      + setting5: {',
-      '            key5: value5',
-      '        }',
-      '    }',
-      '    group1: {',
-      '      - baz: bas',
-      '      + baz: bars',
-      '        foo: bar',
-      '      - nest: {',
-      '            key: value',
-      '        }',
-      '      + nest: str',
-      '    }',
-      '  - group2: {',
-      '        abc: 12345',
-      '    }',
-      '  + group3: {',
-      '        fee: 100500',
-      '    }',
-      '}',
-    ].join('\n');
-    const compare = genCompare('-deep.json');
-    expect(compare).toBe(rightDeepCompare);
-  });
-  it('should generate plain compare', () => {
-    const correctCompare = [
-      'Property \'common.setting2\' was removed',
-      'Property \'common.setting3\' was updated. From true to [complex value]',
-      'Property \'common.setting6.ops\' was added with value: \'vops\'',
-      'Property \'common.follow\' was added with value: false',
-      'Property \'common.setting4\' was added with value: \'blah blah\'',
-      'Property \'common.setting5\' was added with value: [complex value]',
-      'Property \'group1.baz\' was updated. From \'bas\' to \'bars\'',
-      'Property \'group1.nest\' was updated. From [complex value] to \'str\'',
-      'Property \'group2\' was removed',
-      'Property \'group3\' was added with value: [complex value]',
-    ].join('\n');
-    const compare = genCompare('-deep.json', outputFormats.plain);
-    expect(compare).toBe(correctCompare);
-  });
-  it('should generate json compare', () => {
-    const correctStr = [
-      '{',
-      '  "value": {',
-      '    "common": {',
-      '      "value": {',
-      '        "setting1": {',
-      '          "value": "Value 1",',
-      '          "prev": "Value 1",',
-      '          "status": "unmodified"',
-      '        },',
-      '        "setting2": {',
-      '          "prev": 200,',
-      '          "status": "deleted"',
-      '        },',
-      '        "setting3": {',
-      '          "value": {',
-      '            "key": "value"',
-      '          },',
-      '          "prev": true,',
-      '          "status": "modified"',
-      '        },',
-      '        "setting6": {',
-      '          "value": {',
-      '            "key": {',
-      '              "value": "value",',
-      '              "prev": "value",',
-      '              "status": "unmodified"',
-      '            },',
-      '            "ops": {',
-      '              "value": "vops",',
-      '              "status": "added"',
-      '            }',
-      '          },',
-      '          "status": "deep"',
-      '        },',
-      '        "follow": {',
-      '          "value": false,',
-      '          "status": "added"',
-      '        },',
-      '        "setting4": {',
-      '          "value": "blah blah",',
-      '          "status": "added"',
-      '        },',
-      '        "setting5": {',
-      '          "value": {',
-      '            "key5": "value5"',
-      '          },',
-      '          "status": "added"',
-      '        }',
-      '      },',
-      '      "status": "deep"',
-      '    },',
-      '    "group1": {',
-      '      "value": {',
-      '        "baz": {',
-      '          "value": "bars",',
-      '          "prev": "bas",',
-      '          "status": "modified"',
-      '        },',
-      '        "foo": {',
-      '          "value": "bar",',
-      '          "prev": "bar",',
-      '          "status": "unmodified"',
-      '        },',
-      '        "nest": {',
-      '          "value": "str",',
-      '          "prev": {',
-      '            "key": "value"',
-      '          },',
-      '          "status": "modified"',
-      '        }',
-      '      },',
-      '      "status": "deep"',
-      '    },',
-      '    "group2": {',
-      '      "prev": {',
-      '        "abc": 12345',
-      '      },',
-      '      "status": "deleted"',
-      '    },',
-      '    "group3": {',
-      '      "value": {',
-      '        "fee": 100500',
-      '      },',
-      '      "status": "added"',
-      '    }',
-      '  },',
-      '  "status": "deep"',
-      '}',
-    ].join('\n');
-    const compare = genCompare('-deep.json', outputFormats.json);
-    expect(compare).toBe(correctStr);
+
+  describe('should be able to return output of different formats', () => {
+    test('should return stylish output', () => {
+      const expected = fs.readFileSync(
+        `${__dirname}/../__fixtures__/stylish.txt`,
+        'utf-8',
+      );
+      expect(genCompare('.json', outputFormats.stylish)).toEqual(expected);
+    });
+
+    test('should return plain output', () => {
+      const expected = fs.readFileSync(
+        `${__dirname}/../__fixtures__/plain.txt`,
+        'utf-8',
+      );
+      expect(genCompare('.json', outputFormats.plain)).toEqual(expected);
+    });
+
+    test('should throw an exception to an unknown output format', () => {
+      expect(() => genCompare('.json', 'unknown')).toThrow('Unknown format unknown');
+    });
   });
 });
