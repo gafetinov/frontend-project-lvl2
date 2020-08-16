f9d02ea7baa9f1d9218b5c580cadcf1dc27c6703
gafetinov <artem.gafetinov@gmail.com>
2020-08-16 15:03:31 +0500

    Refactor stylish

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -13,8 +13,8 @@ describe('compareFiles', () => {
   const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
 
   it('should display with different formats', () => {
-    expect(genCompare('.json')).toBe(readFile('json.txt'));
-    expect(genCompare('.json', 'stylish')).toBe(readFile('stylish.txt'));
+    expect(genCompare('.json')).toBe(readFile('stylish.txt'));
+    expect(genCompare('.json', 'json')).toBe(readFile('json.txt'));
     expect(genCompare('.json', 'plain')).toBe(readFile('plain.txt'));
   });
 
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,57 +3,63 @@ import { fieldStatuses, getType, types } from '../shared.js';
 
 const INDENT = 4;
 
-const toStringLines = (value, valueName = '', nestingLevel = 0) => {
-  const indent = ' '.repeat(nestingLevel * INDENT);
-  const head = `${indent}${valueName}${valueName ? ': ' : ''}`;
-  const type = getType(value);
-  if (type === types.array) {
-    const content = value.join(', ');
-    return [`${head}[${content}]`];
-  }
-  if (type === types.object) {
-    const content = Object.keys(value)
-      .map((key) => toStringLines(value[key], key, nestingLevel + 1));
-    const end = `${indent}}`;
-    return [`${head}{`, ...content, end];
-  }
-  return [`${head}${value}`];
+const toStringLines = (initialValue, initialKey) => {
+  const iter = (value, valueName, depth) => {
+    const indent = ' '.repeat(depth * INDENT);
+    const head = `${indent}${valueName}${valueName ? ': ' : ''}`;
+    const type = getType(value);
+    if (type === types.array) {
+      const content = value.join(', ');
+      return [`${head}[${content}]`];
+    }
+    if (type === types.object) {
+      const content = Object.keys(value)
+        .map((key) => iter(value[key], key, depth + 1));
+      const end = `${indent}}`;
+      return [`${head}{`, ...content, end];
+    }
+    return [`${head}${value}`];
+  };
+  return iter(initialValue, initialKey, 0);
 };
 
-const getComparedFieldStr = (key, value, indent = '', sign = ' ') => {
+const getMarkedField = (key, value, indent = '', sign = ' ') => {
   const lines = toStringLines(value, key);
   const firstLine = `${indent.slice(0, -2)}${sign} ${lines[0]}`;
   const nextLines = lines.slice(1).map((line) => `${indent}${line}`);
   return [firstLine, ...nextLines].join('\n');
 };
 
-const getFieldCompare = (fieldName, compare, indent) => {
+const stylishFieldCompare = (fieldName, compare, indent) => {
   const { value, prev, status } = compare;
   if (status === fieldStatuses.added) {
-    return getComparedFieldStr(fieldName, value, indent, '+');
+    return getMarkedField(fieldName, value, indent, '+');
   }
   if (status === fieldStatuses.deleted) {
-    return getComparedFieldStr(fieldName, prev, indent, '-');
+    return getMarkedField(fieldName, prev, indent, '-');
   }
   if (status === fieldStatuses.modified) {
-    const deleted = getComparedFieldStr(fieldName, prev, indent, '-');
-    const added = getComparedFieldStr(fieldName, value, indent, '+');
+    const deleted = getMarkedField(fieldName, prev, indent, '-');
+    const added = getMarkedField(fieldName, value, indent, '+');
     return [deleted, added].join('\n');
   }
-  return getComparedFieldStr(fieldName, value, indent);
+  return getMarkedField(fieldName, value, indent);
 };
 
-const stylish = (compare, nestingLevel = 0, fieldName = '') => {
-  const { value, status } = compare;
-  const indent = ' '.repeat(nestingLevel * INDENT);
-  const head = `${indent}${fieldName}${fieldName ? ': ' : ''}`;
-  if (status !== fieldStatuses.deep) {
-    return getFieldCompare(fieldName, compare, indent);
-  }
-  const keys = Object.keys(value);
-  const innerStylish = keys.map((key) => stylish(value[key], nestingLevel + 1, key));
-  const end = `${indent}}`;
-  return [`${head}{`, ...innerStylish, end].join('\n');
+const stylish = (initialCompare) => {
+  const iter = (compare, nestingLevel, fieldName) => {
+    const { value, status } = compare;
+    const indent = ' '.repeat(nestingLevel * INDENT);
+    const head = `${indent}${fieldName}${fieldName ? ': ' : ''}`;
+    if (status !== fieldStatuses.deep) {
+      return stylishFieldCompare(fieldName, compare, indent);
+    }
+    const keys = Object.keys(value);
+    const innerStylish = keys.map((key) => iter(value[key], nestingLevel + 1, key));
+    const end = `${indent}}`;
+    return [`${head}{`, ...innerStylish, end].join('\n');
+  };
+  return iter(initialCompare, 0, '');
 };
 
 export default stylish;
