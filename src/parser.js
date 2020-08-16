be445228d55df682c918598c69e351619cc3e55f
gafetinov <artem.gafetinov@gmail.com>
2020-08-16 15:18:30 +0500

    Refactor parser

--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,15 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (string, format) => {
+export default (str, format) => {
   switch (format) {
     case '.json':
-      return JSON.parse(string);
+      return JSON.parse(str);
     case '.yaml':
     case '.yml':
-      return yaml.safeLoad(string);
+      return yaml.safeLoad(str);
     case '.ini':
-      return ini.parse(string);
+      return ini.parse(str);
     default:
       throw new Error(`Format "${format}" not supported`);
   }
